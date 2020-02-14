23fce130322f8b9ff88ef1dac22903dcce1cb245
Will_Pc_Rampoldi <w.depalo@rampoldixray.it>
2020-02-14 09:45:10 +0100

    allineamento

--- build-xraypanel-Desktop_Qt_5_12_5_MSVC2017_64bit-Debug/debug/moc_serialterminal.cpp
+++ build-xraypanel-Desktop_Qt_5_12_5_MSVC2017_64bit-Debug/debug/moc_serialterminal.cpp
@@ -20,6 +20,135 @@
 QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_StringParsing_t {
+    QByteArrayData data[5];
+    char stringdata0[38];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_StringParsing_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_StringParsing_t qt_meta_stringdata_StringParsing = {
+    {
+QT_MOC_LITERAL(0, 0, 13), // "StringParsing"
+QT_MOC_LITERAL(1, 14, 7), // "process"
+QT_MOC_LITERAL(2, 22, 0), // ""
+QT_MOC_LITERAL(3, 23, 1), // "a"
+QT_MOC_LITERAL(4, 25, 12) // "errorMessage"
+
+    },
+    "StringParsing\0process\0\0a\0errorMessage"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_StringParsing[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       1,   22, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // methods: name, argc, parameters, tag, flags
+       1,    1,   19,    2, 0x02 /* Public */,
+
+ // methods: parameters
+    QMetaType::QString, QMetaType::QString,    3,
+
+ // properties: name, type, flags
+       4, QMetaType::QString, 0x00095001,
+
+       0        // eod
+};
+
+void StringParsing::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<StringParsing *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: { QString _r = _t->process((*reinterpret_cast< QString(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< QString*>(_a[0]) = std::move(_r); }  break;
+        default: ;
+        }
+    }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty) {
+        auto *_t = static_cast<StringParsing *>(_o);
+        Q_UNUSED(_t)
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: *reinterpret_cast< QString*>(_v) = _t->errorMessage(); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::WriteProperty) {
+    } else if (_c == QMetaObject::ResetProperty) {
+    }
+#endif // QT_NO_PROPERTIES
+}
+
+QT_INIT_METAOBJECT const QMetaObject StringParsing::staticMetaObject = { {
+    &QObject::staticMetaObject,
+    qt_meta_stringdata_StringParsing.data,
+    qt_meta_data_StringParsing,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *StringParsing::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *StringParsing::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_StringParsing.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int StringParsing::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty || _c == QMetaObject::WriteProperty
+            || _c == QMetaObject::ResetProperty || _c == QMetaObject::RegisterPropertyMetaType) {
+        qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyDesignable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyScriptable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyStored) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyEditable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyUser) {
+        _id -= 1;
+    }
+#endif // QT_NO_PROPERTIES
+    return _id;
+}
 struct qt_meta_stringdata_SerialTerminal_t {
     QByteArrayData data[17];
     char stringdata0[216];
